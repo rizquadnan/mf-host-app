6f46a7dbf24aef161c6ca4f4d9bd69b775a50d51
rizquadnan <madnanrizqullah@gmail.com>
2023-09-08 22:30:47 +0300

    add dynamic page test

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -10,7 +10,24 @@ export const Navbar = () => {
           <Link href="/">Home</Link>
         </li>
         <li className={styles.listItem}>
-          <Link href="/">Dynamic Page</Link>
+          <Link href="/remote-one-components/one">
+            DynamicPageTest/remote-one/one
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/remote-one-components/two">
+            DynamicPageTest/remote-one/two
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/remote-two-components/one">
+            DynamicPageTest/remote-two/one
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/remote-two-components/two">
+            DynamicPageTest/remote-two/two
+          </Link>
         </li>
       </ul>
     </div>
---
+++ src/pages/remote-two-components/[...slug].tsx
@@ -0,0 +1,53 @@
+import { RemoteImporter } from "@/component/RemoteImporter";
+import { Space, Typography } from "antd";
+import { useRouter } from "next/router";
+import React from "react";
+
+const { Title } = Typography;
+const Page = () => {
+  const router = useRouter();
+
+  return (
+    <Space direction="vertical" size="middle">
+      <Title>Dynamic Page Remote Two Components</Title>
+      <p>
+        This test loading remote app from dynamic page.{" "}
+        <span style={{ color: "blue", fontWeight: 600 }}>
+          Slug: {router.query.slug}
+        </span>
+      </p>
+      <Space direction="vertical">
+        <Title level={2}>Remote Components From remote-two</Title>
+        <div>
+          <Title level={3}>Title</Title>
+          <RemoteImporter
+            remote={{
+              scope: "remote-two",
+              module: "./RemoteTwoTitle",
+            }}
+          />
+        </div>
+        <div>
+          <Title level={3}>Remote Component That Has Render Error</Title>
+          <RemoteImporter
+            remote={{
+              scope: "remote-two",
+              module: "./ComponentWithRenderError",
+            }}
+          />
+        </div>
+        <div>
+          <Title level={3}>Remote Component That Has Handler Error</Title>
+          <RemoteImporter
+            remote={{
+              scope: "remote-two",
+              module: "./ComponentWithHandlerError",
+            }}
+          />
+        </div>
+      </Space>
+    </Space>
+  );
+};
+
+export default Page;
