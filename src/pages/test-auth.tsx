46fca6b75ce25a3c05bc0cb5d9fd392a975edd78
rizquadnan <madnanrizqullah@gmail.com>
2023-09-22 15:37:35 +0300

    test public api

--- src/pages/test-auth.tsx
+++ src/pages/test-auth.tsx
@@ -1,35 +1,49 @@
 import { RemoteImporter } from "@/components/RemoteImporter";
 import { TUser, getCurrentUser } from "@/features/auth";
+import { hello } from "@/features/hello";
+import { THelloResponse } from "@/features/hello/type";
 import NotAuthenticatedLayout from "@/layouts/NotAuthenticatedLayout";
 import { Button, Space, Typography } from "antd";
 import React, { ReactElement, useState } from "react";
 
 function TestAuth() {
   const [authAPIRes, setAuthAPIRes] = useState<TUser>();
+  const [publicAPIRes, setPublicAPIRes] = useState<THelloResponse>()
   const handleCallAuthAPI = async () => {
     try {
       const res = await getCurrentUser();
       setAuthAPIRes(res.data);
-    } catch (error) {}
+    } catch (error) {
+      alert("Failed to get current user")
+    }
   };
 
-  const handleCallPublicAPI = () => {};
+  const handleCallPublicAPI = async () => {
+    try {
+      const res = await hello();
+      setPublicAPIRes(res.data)
+    } catch (error) {
+      alert("Failed to call public api")
+    }
+  };
+
   return (
     <Space direction="vertical">
       <Typography.Title>Test Authentication</Typography.Title>
-      <div>
+      <Space direction="vertical">
         <Typography.Title level={2}>On Host App</Typography.Title>
         <Space direction="vertical">
           <Button onClick={handleCallAuthAPI}>Call Authenticated API</Button>
-          {authAPIRes ? <div>{JSON.stringify(authAPIRes)}</div> : null}
+          {authAPIRes ? <pre>{JSON.stringify(authAPIRes)}</pre> : null}
         </Space>
 
         <Space direction="vertical">
-          <Button>Call public API</Button>
+          <Button onClick={handleCallPublicAPI}>Call public API</Button>
+          {publicAPIRes ? <pre>{JSON.stringify(publicAPIRes)}</pre> : null}
         </Space>
-      </div>
+      </Space>
 
-      <div>
+      <Space direction="vertical">
         <Typography.Title level={2}>On Remote One App</Typography.Title>
         <RemoteImporter
           key={1}
@@ -38,9 +52,9 @@ function TestAuth() {
             scope: "remote-one",
           }}
         />
-      </div>
+      </Space>
 
-      <div>
+      <Space direction="vertical">
         <Typography.Title level={2}>On Remote Two App</Typography.Title>
         <RemoteImporter
           key={2}
@@ -49,7 +63,7 @@ function TestAuth() {
             scope: "remote-two",
           }}
         />
-      </div>
+      </Space>
     </Space>
   );
 }
